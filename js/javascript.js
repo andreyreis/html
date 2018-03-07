447109153b6b066d479194dcdeb876af5ec818ac
andreyreis <arr96rj@gmail.com>
2018-03-06 21:51:12 -0300

    Somar

--- js/javascript.js
+++ js/javascript.js
@@ -7,3 +7,16 @@ function clique_botao()
 {
 alert("Você clicou no botão");
 }
+
+function somarValores(){
+	var s1 = document.getElementById("txt1").value;
+	
+	var s2 = document.getElementById("txt2").value;
+	
+	var s3 = parseInt(s1) + parseInt(s2);
+	alert(s3);
+
+	
+
+	
+}
